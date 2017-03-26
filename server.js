45b24485e28a096dee121e3f403594474eb283b0
Maia <raafamaia@gmail.com>
2017-03-26 13:17:49 -0300

    jshint fixes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var http = require('http')
-var fs = require('fs')
-var path = require('path')
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 http.createServer(function(request, response){
     
     console.log('request starting...');
     
-    var filePath = '.' + request.url
+    var filePath = '.' + request.url;
     
-    if(filePath == './'){
+    if(filePath === './'){
         filePath = './index.html';
     }
     
@@ -27,20 +27,20 @@ http.createServer(function(request, response){
             contentType = 'application/json';
             break;
         case '.png':
-            contentType = 'image/png'
+            contentType = 'image/png';
             break;
         case '.jpg':
-            contentType = 'image/jpg'
+            contentType = 'image/jpg';
             break;
         case '.wav':
-            contentType = 'audio/wav'
+            contentType = 'audio/wav';
             break;            
     }
     
     fs.readFile(filePath, function(error, content){
         
         if(error){
-            if(error.code == 'ENOENT'){
+            if(error.code === 'ENOENT'){
                 fs.readFile('./404.html', function(error, content){
                     response.writeHead(200, {'Content-Type': contentType});
                     response.end(content, 'utf-8');
@@ -48,7 +48,7 @@ http.createServer(function(request, response){
             }else{
                 
                 response.writeHead(500);
-                response.end('Error: ' + error)
+                response.end('Error: ' + error);
                 response.end();
             }
             
